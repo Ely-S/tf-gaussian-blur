7664ee159e983017f337033bc6dcc7c65960afd4
Eli Sakov <eli.sakov@ondaka.com>
2020-11-11 14:17:09 -0800

    fix entrypoint

--- build/index.js
+++ build/index.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.blur = exports.getGaussianKernel = void 0;
-const tf = require("@tensorflow/tfjs");
+var tf = require("@tensorflow/tfjs");
 function get1dGaussianKernel(sigma, size) {
     // Generate a 1d gaussian distribution size numbers long
-    const range = tf.range(Math.floor(-size / 2) + 1, Math.floor(size / 2) + 1);
-    const distribution = tf.pow(tf.exp(range.div(-2.0 * (sigma * sigma))), 2);
-    const normalized = distribution.div(tf.sum(distribution));
+    var range = tf.range(Math.floor(-size / 2) + 1, Math.floor(size / 2) + 1);
+    var distribution = tf.pow(tf.exp(range.div(-2.0 * (sigma * sigma))), 2);
+    var normalized = distribution.div(tf.sum(distribution));
     return normalized;
 }
 function get2dGaussianKernel(size, sigma) {
@@ -16,17 +16,19 @@ function get2dGaussianKernel(size, sigma) {
     var kerne1d = get1dGaussianKernel(sigma, size);
     return tf.outerProduct(kerne1d, kerne1d);
 }
-function getGaussianKernel(size = 5, sigma) {
-    return tf.tidy(() => {
+function getGaussianKernel(size, sigma) {
+    if (size === void 0) { size = 5; }
+    return tf.tidy(function () {
         var kerne2d = get2dGaussianKernel(size, sigma);
         var kerne3d = tf.stack([kerne2d, kerne2d, kerne2d]);
         return tf.reshape(kerne3d, [size, size, 3, 1]);
     });
 }
 exports.getGaussianKernel = getGaussianKernel;
-function blur(image, kernel) {
-    return tf.tidy(() => {
-        return tf.depthwiseConv2d(image, kernel, 1, 'valid');
+function blur(image, kernel, pad) {
+    if (pad === void 0) { pad = 'same'; }
+    return tf.tidy(function () {
+        return tf.depthwiseConv2d(image, kernel, 1, pad);
     });
 }
 exports.blur = blur;
